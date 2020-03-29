e6bfa821e9a5b881875fe1ea9947a18795288dec
shantanu354 <kanhapatildurg@gmail.com>
2020-03-29 14:37:16 +0530

    check

--- app/src/main/java/com/mitapp/busify/MainActivity.java
+++ app/src/main/java/com/mitapp/busify/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    boolean logged_in = false; //change this later (check from database)
+    boolean logged_in = false; //change this later (check from firebase)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
