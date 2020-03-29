9d204bb56c69eed981c56da721e88f1ab1e1ce28
shantanu354 <kanhapatildurg@gmail.com>
2020-03-30 00:28:08 +0530

    navigation bar (incomplete)

--- app/src/main/java/com/mitapp/busify/MainActivity.java
+++ app/src/main/java/com/mitapp/busify/MainActivity.java
@@ -1,23 +1,45 @@
 package com.mitapp.busify;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    boolean logged_in = false; //change this later to check from firebase
+
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
+    boolean logged_in = true; //change this later to check from firebase
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(logged_in){
             setContentView(R.layout.activity_main);
+
+            nDrawerLayout = (DrawerLayout) findViewById(R.id.home_page);
+            nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+
+            nDrawerLayout.addDrawerListener(nToggle);
+            nToggle.syncState();
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         else{
             Intent intent = new Intent(MainActivity.this, login_activity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (nToggle.onOptionsItemSelected(item)) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
