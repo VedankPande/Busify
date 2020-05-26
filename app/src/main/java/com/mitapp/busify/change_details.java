463a1ab30263834fb08bca6531e80fe604351118
vedank <vedank.pande@gmail.com>
2020-05-26 11:06:16 +0530

    update details done, stop and bus letter spinners need work

--- app/src/main/java/com/mitapp/busify/change_details.java
+++ app/src/main/java/com/mitapp/busify/change_details.java
@@ -7,14 +7,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
 public class change_details extends AppCompatActivity {
+
+    EditText Name,Stop,Phone;
+    String bus_id;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,29 @@ public class change_details extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Name = findViewById(R.id.signup_name);
+        Stop = findViewById(R.id.details_stop);
+        Phone = findViewById(R.id.signup_phone);
+
+        findViewById(R.id.confirm_changes_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference();
+
+                String name = Name.getText().toString();
+                //String stop = Stop.getText().toString();
+                String phone = Phone.getText().toString();
+                User user = new User(name, "Home", "A", phone);
+                Toast.makeText(change_details.this, "values:"+ " "+name, Toast.LENGTH_SHORT).show();
+                reference.child("users").child(""+name).setValue(user);
+                reference.child("users").child("test").child("value").setValue("helloworld");
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -44,5 +78,6 @@ public class change_details extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
 
 }
