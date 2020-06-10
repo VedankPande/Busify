fe79bfdaf3a913eb04e25aa6fe0b91f9450e9540
shantanu354 <kanhapatildurg@gmail.com>
2020-06-10 21:29:31 +0530

    activity request stop respond fixed

--- app/src/main/java/com/mitapp/busify/Request_Stop_Respond.java
+++ app/src/main/java/com/mitapp/busify/Request_Stop_Respond.java
@@ -22,82 +22,12 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
-public class Request_Stop_Respond extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+public class Request_Stop_Respond extends AppCompatActivity{
     private ActionBarDrawerToggle nToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request__stop__respond);
-
-        //Functions for check boxes
-        RadioGroup main_menu_radio_group = findViewById(R.id.main_menu_radio_group);
-        main_menu_radio_group.bringToFront();
-
-        findViewById(R.id.checkbox_do_not_show_buses).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-        findViewById(R.id.checkbox_show_my_buses).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-        findViewById(R.id.checkbox_show_all_buses).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        //This is for UI
-        Toolbar my_toolbar = findViewById(R.id.passenger_actionBar);
-        my_toolbar.setTitle("");
-        setSupportActionBar(my_toolbar);
-
-        DrawerLayout nDrawerLayout = findViewById(R.id.request_stop_respond_menu);
-        nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
-
-
-        nDrawerLayout.addDrawerListener(nToggle);
-        nToggle.syncState();
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
-        NavigationView navigationView = findViewById(R.id.notif_navView);
-        navigationView.setNavigationItemSelectedListener(this);
     }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (nToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.change_details){
-            startActivity(new Intent(getApplicationContext(), change_details.class));
-        }
-        if (id == R.id.settings){
-            startActivity(new Intent(getApplicationContext(), Settings.class));
-        }
-        if (id == R.id.logout_nav){
-            FirebaseAuth.getInstance().signOut();
-            GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .build()).signOut().addOnSuccessListener(new OnSuccessListener<Void>(){
-                @Override
-                public void onSuccess(Void aVoid){
-                    startActivity(new Intent(getApplicationContext(), login_activity.class));
-                }});
-        }
-        if (id == R.id.app_info){
-            startActivity(new Intent(getApplicationContext(), app_info.class));
-        }
-        return false;
-    }
-
 }
