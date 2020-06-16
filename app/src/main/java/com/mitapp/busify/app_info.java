1b3de6739e809f58af1d0743fa79983c086b4090
vedank <vedank.pande@gmail.com>
2020-06-17 01:19:39 +0530

    linkedin done

--- app/src/main/java/com/mitapp/busify/app_info.java
+++ app/src/main/java/com/mitapp/busify/app_info.java
@@ -3,8 +3,13 @@ package com.mitapp.busify;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -18,6 +23,31 @@ public class app_info extends AppCompatActivity {
         my_toolbar.setTitle("");
         setSupportActionBar(my_toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.linkedin1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                try {
+                    intent.setData(Uri.parse("https://in.linkedin.com/in/pshantanu"));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException exception) {
+                    Toast.makeText(getApplicationContext(), "Error text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        findViewById(R.id.linkedin2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                try {
+                    intent.setData(Uri.parse("https://www.linkedin.com/in/vedank-pande-3b226317b"));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException exception) {
+                    Toast.makeText(getApplicationContext(), "Error text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
